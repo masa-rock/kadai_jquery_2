c8c08455386f0a97faceec8780c423512238c619
EC2 Default User <ec2-user@ip-172-31-17-209.us-west-1.compute.internal>
2022-04-24 08:34:20 +0000

    四則演算コマンドの2度押しバグ修正

--- main.js
+++ main.js
@@ -3,32 +3,44 @@ $(document).ready(function(){
   let push_num = [];
   let result ="";
   let num_1 = "";
-  var num_top="";
+  var receive_num="";
   var len_array ="";
   
   
+  // 数字ボタン操作
   $('.com').on('click',function(){
-    push_num.push($(this).text());
-    num_1 = push_num.join('');
-    num_top = push_num[0];
+    receive_num = $(this).text();
     len_array = push_num.length;
-    if (len_array == 1 && (num_top == 0 || num_top == 00 ||  $(this).hasClass("com_cal"))){
+    $(".com_cal").prop("disabled",false);
+    // 0,00の初回入力防止
+    if (len_array == 0 && (receive_num == 0 || receive_num == 00)){
       $(".calculator_display").text(0);
-      push_num = [];
     }else{
+      push_num.push(receive_num);
+      num_1 = push_num.join('');
       $(".calculator_display").text(num_1);
     }
   });
   
-  // || className == "com_cal"
-  // 四則演算ボタン操作
-  
+  // 四則演算ボタン操作　
+  $('.com_cal').on('click',function(){
+    len_array = push_num.length;
+    if (len_array == 0){
+      $(".calculator_display").text(0);
+    }else{
+      push_num.push($(this).text());
+      num_1 = push_num.join('');
+      $(".calculator_display").text(num_1);
+      $('.com_cal').prop("disabled",true);
+    }
+  });
   
   // ACボタン操作
   $('.com_ac').on('click',function(){
     push_num = [];
     num_1 = "";
     $(".calculator_display").text(0);
+    $(".com_cal").prop("disabled",false);
   });
   
   // equalボタン操作
@@ -36,6 +48,7 @@ $(document).ready(function(){
       result = eval(num_1);
       $(".calculator_display").text(result);
       push_num = [result];
+      $(".com_cal").prop("disabled",false);
   });
 });
 
